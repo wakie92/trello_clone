afcd5111bd8c8c5d6aa071876a25312490e7697d
Changhun <wakie9292@gmail.com>
2020-05-14 22:40:37 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import HeaderLayout from './components/Header/HeaderLayout';
+import CardList from './components/Card/CardList';
 
 function App() {
   const [counter, setCounter] = useState(0);
 
   const onIncrease = () => {
     setCounter(counter + 1);
-  }
+  };
   const onDecrease = () => {
-    setCounter(counter -1 );
-  }
+    setCounter(counter - 1);
+  };
   return (
-    <div>
-      <h1>{counter}</h1>
-      <button type="button" onClick={onIncrease}>+1</button>
-      <button type="button" onClick={onDecrease}>-1</button>
-    </div>
+    <AppWrapper>
+      <HeaderLayout />
+      <CardList />
+    </AppWrapper>
   );
 }
+
+const AppWrapper = styled.div`
+  background-image: url('https://blog-oscar.s3.ap-northeast-2.amazonaws.com/images/himalayas.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  background-position: 50%;
+  height: 100vh;
+  position: relative;
+`;
 
 export default App;
